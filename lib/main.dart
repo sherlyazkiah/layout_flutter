26d5b8bab81d8a1a96603fe64296378d9ccbee41
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-10-05 22:15:18 +0700

    practicum 4

--- lib/main.dart
+++ lib/main.dart
@@ -80,8 +80,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'img/eiffel.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
