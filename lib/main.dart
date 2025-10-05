3c84ff177fc3d8a9504ae01851ba5e0227ee1a26
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-10-05 21:59:10 +0700

    practicum 3

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Title Section
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -20,14 +21,14 @@ class MyApp extends StatelessWidget {
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: const Text(
-                    'Wisata Gunung di Batu',
+                    'Eiffel Tower',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 Text(
-                  'Batu, Malang, Indonesia',
+                  'Paris, France',
                   style: TextStyle(
                     color: Colors.grey,
                   ),
@@ -45,6 +46,7 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    // Button Section
     Color color = Theme.of(context).primaryColor;
 
     Widget buttonSection = Row(
@@ -56,6 +58,22 @@ class MyApp extends StatelessWidget {
       ],
     );
 
+    // Text Section
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'The Eiffel Tower is one of the most iconic landmarks in the world, '
+        'located in the heart of Paris, France. Standing at 330 meters tall, '
+        'it was originally built as the entrance arch for the 1889 World’s Fair. '
+        'Today, the tower attracts millions of visitors each year who come to '
+        'enjoy its breathtaking views of Paris, romantic atmosphere, and dazzling night lights. '
+        'Whether admired from below or from its observation decks, the Eiffel Tower remains '
+        'a timeless symbol of love, art, and architectural brilliance.'
+        '\n\nCreated by: Sherly Lutfi Azkiah Sulistyawati 2341720241',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Sherly Lutfi Azkiah Sulistyawati 2341720241',
       home: Scaffold(
@@ -65,7 +83,8 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
-            buttonSection
+            buttonSection,
+            textSection,
            ],
         ),
       ),
